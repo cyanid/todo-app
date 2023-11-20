8c3b99ba63de12d6c1dcceb44b9394930e35148f
Teemu Huttunen <teemu@huttunen.fi>
2023-11-20 08:25:57 +0200

    Validate form and display errors

--- src/todos/TodoForm.tsx
+++ src/todos/TodoForm.tsx
@@ -9,8 +9,13 @@ interface TodoFormProps {
 
 function TodoForm({ onCancel, onSave, todo: initialTodo }: TodoFormProps) {
     const [todo, setTodo] = useState<Todo>(initialTodo);
+    const [errors, setErrors] = useState({
+        title: '',
+        description: '',
+    });
     const handleSubmit = (event: SyntheticEvent) => {
             event.preventDefault();
+            if (!isValid()) return;
             onSave(todo);
     };
     const handleChange = (event: any) => {
@@ -32,14 +37,45 @@ function TodoForm({ onCancel, onSave, todo: initialTodo }: TodoFormProps) {
             updatedTodo = new Todo({ ...t, ...change });
             return updatedTodo;
         });
+        setErrors(() => validate(updatedTodo));
     };
+    function validate(todo: Todo) {
+        let errors: any = { title: '', description: '' };
+        if (todo.title.length === 0) {
+            errors.title = 'Title is required';
+        }
+        if (todo.title.length > 0 && todo.title.length < 3) {
+            errors.title = 'Title needs to be at least 3 characters.';
+        }
+        if (todo.description.length === 0) {
+            errors.description = 'Description is required.';
+        }
+        return errors;
+    }
+        
+    function isValid() {
+        return (
+            errors.title.length === 0 &&
+            errors.description.length === 0
+        );
+    }
     
   return (
     <form className="input-group vertical" onSubmit={handleSubmit}>
       <label htmlFor="name">Todo Title</label>
       <input type="text" name="title" placeholder="enter title" value={todo.title} onChange={handleChange} />
+      {errors.title.length > 0 && (
+        <div className="card error">
+          <p>{errors.title}</p>
+        </div>
+      )}
       <label htmlFor="description">Todo Description</label>
       <textarea name="description" placeholder="enter description" value={todo.description} onChange={handleChange} />
+      {errors.description.length > 0 && (
+        <div className="card error">
+          <p>{errors.description}</p>
+        </div>
+      )}
       <div className="input-group">
         <button className="primary bordered medium" type="submit">Save</button>
         <span />
